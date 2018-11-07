6f7473152fd323669d49a78abbdb7e98190c370b
unknown <sallevu32@gmail.com>
2018-11-07 13:24:25 +0100

    Changed Validation Form

--- javascript/form.js
+++ javascript/form.js
@@ -1,37 +1,54 @@
-function submit(){
-    console.log("test");
-    validateform();
-    document.getElementById("form1").submit();
-}
+(function() {
 
-function reset(){
-    console.log("test2");
-    document.getElementById("form1").reset();
-}
-
-function validateform(){
-    var fname = document.getElementById('fname').value;
-    if (fname === ""){
-        alert("Enter first name please!");
-        return false;
+    function clearForm(){
+        document.getElementById("form1").reset();
     }
-
-    var lname = document.getElementById('lname').value;
-    if (lname === ""){
-        alert("Enter last name please!");
-        return false;
+    
+    function sendMail(){
+        var mail = document.getElementById("form1");
+        document.getElementById("form1").action="mailto:Sasa.Djapic.STUDENT@unicreditgroup.zaba.hr?subject=subject&body=body";
+        document.getElementById("form1").submit();
     }
-
-    var email = document.getElementById('email').value;
-    if (email === ""){
-        alert("Enter E-mail address please!");
-        return false;
+    
+    var myfunction = function(event){
+    event.preventDefault();
+    validateForm();
+    sendMail();
+    
     }
+    var form = document.getElementById("form1");
+    
+    form.addEventListener("submit", myfunction, true);
+    
+    var clearBtn = document.getElementById("clr");
+        
+    clearBtn.addEventListener("click", clearForm);
 
-    var phone = document.getElementById('phone').value;
-    if (phone === ""){
-        alert("Enter phone number please!");
-        return false;
+    function validateForm(){
+
+        var fname = document.getElementById('fname').value;
+        if (fname === ""){
+            alert("Enter first name please!");
+            return false;
+        }
+    
+        var lname = document.getElementById('lname').value;
+        if (lname === ""){
+            alert("Enter last name please!");
+            return false;
+        }
+    
+        var email = document.getElementById('email').value;
+        if (email === ""){
+            alert("Enter E-mail address please!");
+            return false;
+        }
+    
+        var phone = document.getElementById('phone').value;
+        if (phone === ""){
+            alert("Enter phone number please!");
+            return false;
+        }
+        var mess = document.getElementById('mess').value;
     }
-    var mess = document.getElementById('mess').value;
-}
+})();
